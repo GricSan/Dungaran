3901182abdf44be8e833e211104c2b5208a64145
Aleksejs Gricisins <aleksejs.gricisins@contractors.roche.com>
2022-07-15 12:40:15 +0300

    Gradle clean-ups; Initial Clean Architecture structure created;

--- app/src/main/java/org/gricsan/dungaran/MainActivity.kt
+++ app/src/main/java/org/gricsan/dungaran/MainActivity.kt
@@ -1,13 +1,25 @@
 package org.gricsan.dungaran
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.gricsan.dungaran.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
